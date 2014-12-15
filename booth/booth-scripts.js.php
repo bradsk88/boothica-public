7bba821a943db8930077da0af334250f28cbdef5
Brad <bradsk88@gmail.com>
2014-12-14 23:41:49 -0600

    I don't even remember.

--- booth/booth-scripts.js.php
+++ booth/booth-scripts.js.php
@@ -15,7 +15,7 @@ $base = base();
 
 echo <<<EOT
 
-var debugging = false;
+var debugging;debugging=false;
 var boother;
 
 function getDataType() {
@@ -30,17 +30,18 @@ function openBooth(boothnum) {
     window.boothnumber = boothnum;
     window.scrollTo(0, 0);
 
-    $("head").append("<link rel= \"stylesheet \" href= \"$base/css/commentinput.css \" type= \"text/css \" media= \"screen \" />");
-    $("head").append("<link rel= \"stylesheet \" href= \"$base/css/booth.css \" type= \"text/css \" media= \"screen \" />");
-    $("head").append("<script type =  \"text/javascript \" src =  \"$base/common/jquery.a-tools-1.5.2.min.js \"></script>");
-    $("head").append("<script type =  \"text/javascript \" src =  \"$base/common/jquery.asuggest.js \"></script>");
+    $("head").append("<link rel= 'stylesheet' href= '$base/css/commentinput.css' type= 'text/css' media= 'screen' />");
+    $("head").append("<link rel= 'stylesheet' href= '$base/css/booth.css' type= 'text/css' media= 'screen' />");
+    $("head").append("<script type =  'text/javascript' src = '$base/common/jquery.a-tools-1.5.2.min.js'></script>");
+    $("head").append("<script type =  'text/javascript' src = '$base/common/jquery.asuggest.js'></script>");
+    $("head").append("<script type =  'text/javascript' src = 'http://ajax.googleapis.com/ajax/libs/swfobject/2.2/swfobject.js'></script>");
     $("#centerpane").html(
-        "<div class = \"centersection\" id=\"boothtop\"></div>" +
-            "<div class = \"centersection\" id=\"boothcomments\"></div>" +
-            "<iframe class = \"centersection\" id=\"comment_target\" name = \"comment_target\" src = \"$base/comment/target.html\"  style=\"width:100%;height:0;border:0 solid #FFF;\"></iframe>" +
-            "<div class = \"centersection\" id=\"boothcommentinput\">");
-    $("#boothtop").html("<div class =  \"loadspinner \"></div>");
-    $("#boothcomments").html("<div class =  \"loadspinner \"></div>");
+        "<div class = 'centersection' id='boothtop'></div>" +
+            "<div class = 'centersection' id= 'boothcomments'></div>" +
+            "<iframe class = 'centersection' id= 'comment_target' name = 'comment_target' src = '$base/comment/target.html'  style= 'width:100%;height:0;border:0 solid #FFF;'></iframe>" +
+            "<div class = 'centersection' id='boothcommentinput'>");
+    $("#boothtop").html("<div class = 'loadspinner'></div>");
+    $("#boothcomments").html("<div class = 'loadspinner'></div>");
 
     reloadBoothBody(boothnum, function () {
         reloadBoothCommentInput(boothnum, function () {
@@ -88,9 +89,9 @@ function startBoothEdit(boothnum) {
     }, getDataType())
         .fail(function (jqXHR, textStatus, errorThrown) {
             $("#boothblurb").append(
-                "<div class = \"centersection\">" +
+                "<div class = 'centersection'>" +
                     "There was a problem... [" + textStatus + "]" +
-                    "<div class =  \"sectionrefresh\" onclick= \"reloadBoothBody(" + boothnum + ")\"></div>" +
+                    "<div class = 'sectionrefresh' onclick= 'reloadBoothBody(" + boothnum + ")'></div>" +
                     "</div>");
         });
 
@@ -101,19 +102,19 @@ function getBlurbEditHTML(data) {
     $.each(data, function (idx, obj) {
 		var blu = obj.blurb.replace("/<br ?\/?>/g", "\\n")
         html = html +
-            "<textarea class = \"blurbedit\" id = \"blurbedit\">" +
+            "<textarea class = 'blurbedit' id = 'blurbedit'>" +
                 blu +
             "</textarea>" +
-            "<div id = \"changeblurbbutton\" class = \"plainbutton plainbuttonright tallbutton\" onclick= \"commitEdit("+obj.boothnum+") \">" +
+            "<div id = 'changeblurbbutton' class = 'plainbutton plainbuttonright tallbutton' onclick= 'commitEdit("+obj.boothnum+")'>" +
                 "Submit Changes" +
             "</div>" +
-            "<div class = \"wideboothpad\"></div>"
+            "<div class = 'wideboothpad'></div>"
     });
     return html;
 }
 
 function commitEdit(boothnum) {
-    $("#boothcommentinput").html("<div class = \"loadspinner\"></div>");
+    $("#boothcommentinput").html("<div class = 'loadspinner'></div>");
     var newBlurb = $("#blurbedit").val();
     $.post("$base/_mobile/changeblurb.php", {
         boothnum: boothnum,
@@ -133,7 +134,7 @@ function commitEdit(boothnum) {
     },"json")
         .fail(function (jqXHR, textStatus, errorThrown) {
             $("#boothcommentinput").html("There was a problem... [" + textStatus + ":" + errorThrown + "]" +
-                "<div class =  \"sectionrefresh \" onclick= \"reloadBoothCommentInput(" + boothnum + ") \"></div>");
+                "<div class = 'sectionrefresh' onclick= 'reloadBoothCommentInput(" + boothnum + ")'></div>");
         })
 }
 
@@ -149,7 +150,7 @@ function reloadBoothCommentInput(boothnum, thenDo) {
     })
         .fail(function (jqXHR, textStatus, errorThrown) {
             $("#boothcommentinput").html("There was a problem... [" + textStatus + ":" + errorThrown + "]" +
-                "<div class =  \"sectionrefresh \" onclick= \"reloadBoothCommentInput(" + boothnum + ") \"></div>");
+                "<div class = 'sectionrefresh' onclick= 'reloadBoothCommentInput(" + boothnum + ")'></div>");
         })
 }
 
@@ -168,7 +169,7 @@ function reloadBoothBody(boothnum, thenDo) {
     }, getDataType())
         .fail(function (jqXHR, textStatus, errorThrown) {
             $("#boothtop").html("There was a problem... [" + textStatus + "]" +
-                "<div class =  \"sectionrefresh \" onclick= \"reloadBoothBody(" + boothnum + ") \"></div>");
+                "<div class = 'sectionrefresh' onclick= 'reloadBoothBody(" + boothnum + ")'></div>");
         });
 }
 
@@ -207,7 +208,7 @@ function reloadBoothComments(boothnum) {
     }, getDataType())
         .fail(function (jqXHR, textStatus, errorThrown) {
             $("#boothcomments").html("There was a problem... [" + textStatus + ":" + errorThrown + "]" +
-                "<div class =  \"sectionrefresh \" onclick= \"reloadBoothComments(" + boothnum + ") \"></div>");
+                "<div class = 'sectionrefresh' onclick= 'reloadBoothComments(" + boothnum + ")'></div>");
         });
 }
 
@@ -225,36 +226,36 @@ function getBoothHTML(data) {
             window.history.pushState("string", "Title", newLocation);
         }
         html =
-            "<div style =  \"float: left \">" +
-                "<a onclick= \"backToMain() \">" +
+            "<div style = 'float: left'>" +
+                "<a onclick= 'backToMain()'>" +
                 "Go Back" +
                 "</a>" +
                 "</div>" +
-                "<div style =  \"position: absolute; right: 0; \" width: 100px;>" +
-                "<a onclick=\"openPMsForUser( \"" + obj.boothername + " \")\">" +
+                "<div style = 'position: absolute; right: 0;' width: 100px;>" +
+                "<a onclick = 'openPMsForUser(\"" + obj.boothername + "\")'>" +
                 "Send PM" +
                 "</a>" +
                 "</div>" +
-                "<div class =  \"navbuttons \">";
+                "<div class = 'navbuttons'>";
         if ("undefined" === typeof(obj.prevnum) || obj.prevnum == null) {
             html = html +
-                "<div class = \"navbuttonleft plainbutton standardbutton\" style = \"cursor:default\">&nbsp;</div>" +
-                "<div class = \"navbutton plainbutton standardbutton\" style = \"cursor:default\">&nbsp;</div>";
+                "<div class = 'navbuttonleft plainbutton standardbutton' style = 'cursor:default'>&nbsp;</div>" +
+                "<div class = 'navbutton plainbutton standardbutton' style = 'cursor:default'>&nbsp;</div>";
         } else {
             html = html +
-                "<div class = \"navbuttonleft plainbutton standardbutton\" onclick=\"openBooth(" + obj.firstnum + ")\">Oldest</div>" +
-                "<div class = \"navbutton plainbutton standardbutton\" onclick=\"openBooth(" + obj.prevnum + ")\">Prev</div>";
+                "<div class = 'navbuttonleft plainbutton standardbutton' onclick='openBooth(" + obj.firstnum + ")'>Oldest</div>" +
+                "<div class = 'navbutton plainbutton standardbutton' onclick='openBooth(" + obj.prevnum + ")'>Prev</div>";
         }
         html = html +
-            "<div class = \"navbuttoncenter plainbutton standardbutton\" onclick=\"openUserFeed( \"" + obj.boothername + " \")\">" + obj.bootherdisplayname + "</div>";
+            "<div class = 'navbuttoncenter plainbutton standardbutton' onclick='openUserFeed(\"" + obj.boothername + "\")'>" + obj.bootherdisplayname + "</div>";
         if ("undefined" === typeof(obj.nextnum) || obj.nextnum == null) {
             html = html +
-                "<div class = \"navbutton plainbutton standardbutton\" style = \"cursor:default\">&nbsp;</div>" +
-                "<div class = \"navbuttonright plainbutton standardbutton\" style = \"cursor:default\">&nbsp;</div>";
+                "<div class = 'navbutton plainbutton standardbutton' style = 'cursor:default'>&nbsp;</div>" +
+                "<div class = 'navbuttonright plainbutton standardbutton' style = 'cursor:default'>&nbsp;</div>";
         } else {
             html = html +
-                "<div class = \"navbutton plainbutton standardbutton\" onclick=\"openBooth(" + obj.nextnum + ")\">Next</div>" +
-                "<div class = \"navbuttonright plainbutton standardbutton\" onclick=\"openBooth(" + obj.lastnum + ")\">Newest</div>";
+                "<div class = 'navbutton plainbutton standardbutton' onclick='openBooth(" + obj.nextnum + ")'>Next</div>" +
+                "<div class = 'navbuttonright plainbutton standardbutton' onclick='openBooth(" + obj.lastnum + ")'>Newest</div>";
         }
 
         var cellClass = "wideboothcell";
@@ -262,22 +263,22 @@ function getBoothHTML(data) {
             cellClass = "wideboothcell-tall";
         }
         html = html +
-            "<div style = \"clear: both\"></div>" +
+            "<div style = 'clear: both'></div>" +
             "</div>" +
-            "<div class =  \"sectionrefresh \" onclick= \"openBooth(" + obj.boothnum + ") \"></div>" +
-            "<div style =  \"clear: both; \"></div>" +
-            "<div class =  \"widebooth \">" +
-            "<div class =  \""+cellClass+" \">" +
-            "<div class = \"wideboothdatestamp\">" + obj.datetime + "-UTC</div>"+ //TODO: use user \"s zone
-            "<img class = \"wideboothimg\" src =  \"" + obj.imagePath + " \" style =  \"width: 100% \">" +
+            "<div class = 'sectionrefresh' onclick= 'openBooth(" + obj.boothnum + ")'></div>" +
+            "<div style = 'clear: both;'></div>" +
+            "<div class = 'widebooth'>" +
+            "<div class = '"+cellClass+"'>" +
+            "<div class = 'wideboothdatestamp'>" + obj.datetime + "-UTC</div>"+ //TODO: use user "s zone
+            "<img class = 'wideboothimg' src = '" + obj.imagePath + "' style =  'width: 100%'>" +
             "</div>" +
-            "<div class =  \"wideboothbuttons plainbutton standardbutton \">" +
-            "<div class =  \"wideboothbuttonshalf wideboothbuttonslefthalf \">" +
+            "<div class = 'wideboothbuttons plainbutton standardbutton'>" +
+            "<div class = 'wideboothbuttonshalf wideboothbuttonslefthalf'>" +
             "Booth " + obj.userboothcount;
         if (obj.userboothcount != obj.userboothnum) {
             var discrepencyMsg = obj.bootherdisplayname + " has actually posted " + obj.userboothnum + " booths, but they have deleted some.";
             html = html +
-                "<span title = \"" + discrepencyMsg + "\">" +
+                "<span title = '" + discrepencyMsg + "'>" +
                 " * " +
                 "</span>";
         }
@@ -286,31 +287,31 @@ function getBoothHTML(data) {
             style = "display: inherited;";
         }
         html = html +
-            "<div class =  \"bootheffect \" id =  \"bootheffect \"></div>" +
-            "<div class = \"boothlikeslabel\">Likes:</div>" +
-            "<div class =  \"boothlikewrap \" id =  \"boothlikewrap \" style = \"" + style + "\">" +
-            "<div class = \"boothlike\" id =  \"boothlike \" title = \"" + obj.likes + " Likes\">" +
+            "<div class = 'bootheffect' id = 'bootheffect'></div>" +
+            "<div class = 'boothlikeslabel'>Likes:</div>" +
+            "<div class = 'boothlikewrap' id = 'boothlikewrap' style = '" + style + "'>" +
+            "<div class = 'boothlike' id = 'boothlike' title = '" + obj.likes + " Likes'>" +
             obj.likes +
             "</div>" +
             "</div>" +
             "</div>" +
-            "<div class =  \"wideboothbuttonshalf \">" +
-            "<div class =  \"boothbutton \" onclick= \"likeBooth(" + obj.boothnum + ") \">Like</div>";
+            "<div class = 'wideboothbuttonshalf'>" +
+            "<div class = 'boothbutton' onclick= 'likeBooth(" + obj.boothnum + ")'>Like</div>";
         if (window.username == obj.boothername) {
             html = html +
-                "<div class =  \"boothbutton \" onclick= \"startBoothEdit(" + obj.boothnum + ") \">Edit</div>" +
+                "<div class = 'boothbutton' onclick= 'startBoothEdit(" + obj.boothnum + ")'>Edit</div>" +
                 //TODO: Use AJAX for booth delete
-                "<a href =  \"$base/actions/deletebooth?number=" + obj.boothnum + " \"><div class = \"boothbutton \">Delete</div></a>";
+                "<a href = '$base/actions/deletebooth?number=" + obj.boothnum + "'><div class = ''boothbutton'>Delete</div></a>";
         }
         html = html +
-            "<div style = \"clear: both\"></div>" +
+            "<div style = 'clear: both'></div>" +
             "</div>" +
-            "<div style = \"clear: both\"></div>" +
+            "<div style = 'clear: both'></div>" +
             "</div>" +
-            "<div class =  \"wideboothtext \" id = \"boothblurb\">" + obj.blurb + "</div>" +
+            "<div class = 'wideboothtext' id = 'boothblurb'>" + obj.blurb + "</div>" +
             "</div>";
         if (!obj.isfriend) {
-            html = html + "<div class =  \"plainbutton tallbutton \" onclick=\"sendFriendRequest( \"" + obj.boothername + " \")\">Send Friend Request</div>";
+            html = html + "<div class = 'plainbutton tallbutton' onclick='sendFriendRequest(\"" + obj.boothername + "\")'>Send Friend Request</div>";
         }
     });
     return html;
@@ -320,8 +321,8 @@ function getBoothCommentsHTML(boothnum, data) {
     if (debugging) {
         return data;
     }
-    $("head").append("<script type =  \"text/javascript \" src =  \"$base/booth/userbooths-scripts.js \"></script>");
-    var html = "<div class =  \"boothcomments \">";
+    $("head").append("<script type = 'text/javascript' src = '$base/booth/userbooths-scripts.js'></script>");
+    var html = "<div class = 'boothcomments'>";
     $.each(data, function (idx, obj) {
 
         var hasImage = false;
@@ -341,19 +342,19 @@ function getBoothCommentsHTML(boothnum, data) {
 
 
         html = html +
-            "<div class =  \"centersection \" style =  \"position: relative; " + sectionHeight + " \">" +
-            "<div class =  \"" + boothcommenttextclass + " \">" +
-            "<div class =  \"boothcommenttime \">" + obj.time + "</div>" +
-            "<div class =  \"boothcommenttextbg \">" +
+            "<div class = 'centersection' style = 'position: relative; " + sectionHeight + "'>" +
+            "<div class = '" + boothcommenttextclass + "'>" +
+            "<div class = 'boothcommenttime'>" + obj.time + "</div>" +
+            "<div class = 'boothcommenttextbg'>" +
             obj.commenttext +
             "</div>" +
             "</div>";
         if (hasImage) {
             html = html +
-                "<div class = \"boothcommenterimageuploaded\" style = \"background-image: url($base" + obj.imageHash + "); height: " + imageHeightInEM + "em;\"></div>";
+                "<div class = 'boothcommenterimageuploaded' style = 'background-image: url($base" + obj.imageHash + "); height: " + imageHeightInEM + "em;'></div>";
         } else {
             html = html +
-                "<div class =  \"boothcommenterimage \" onclick=\"openUserFeed( \"" + obj.commentername + " \")\" style =  \"background-image: url($base" + obj.iconImage + ") \"></div>";
+                "<div class = 'boothcommenterimage' onclick='openUserFeed(\"" + obj.commentername + "\")' style = 'background-image: url($base" + obj.iconImage + ")'></div>";
         }
         var style = "display: none;";
         if ("undefined" !== typeof(obj.likes) && obj.likes != null && obj.likes > 0) {
@@ -366,22 +367,22 @@ function getBoothCommentsHTML(boothnum, data) {
             likeClass = "boothcommenterlikeswithupload";
         }
         html = html +
-            "<div class =  \"" + effectClass + " \" id =  \"effect" + obj.commentnum + " \"></div>" +
-            "<div class =  \"" + likeClass + " \" id =  \"likewrap" + obj.commentnum + " \" style = \"" + style + "\">" +
-            "<div class = \"boothcommentlike\" id =  \"like" + obj.commentnum + " \" title = \"" + obj.likes + " Likes\">" +
+            "<div class = '" + effectClass + "' id = 'effect" + obj.commentnum + "'></div>" +
+            "<div class = '" + likeClass + "' id = 'likewrap" + obj.commentnum + "' style = '" + style + "'>" +
+            "<div class = 'boothcommentlike' id = 'like" + obj.commentnum + "' title = '" + obj.likes + " Likes'>" +
             obj.likes +
             "</div>" +
             "</div>" +
-            "<div class = \"boothcommentername\" onclick=\"openUserFeed('" + obj.commentername + "')\">" + obj.commenterdisplayname + "</div>" +
+            "<div class = 'boothcommentername' onclick='openUserFeed(\"" + obj.commentername + "\")'>" + obj.commenterdisplayname + "</div>" +
 
-            "<div class =  \"boothcommentbuttons \">";
+            "<div class = 'boothcommentbuttons'>";
         if ("undefined" !== typeof(window.username) && window.username.toLowerCase() == boother.toLowerCase() && obj.commentername != window.username) {
-            html = html + "<div class =  \"boothcommentbutton secondcommentbutton \" onclick= \"deleteComment(" + boothnum + ", " + obj.commentnum + ") \">Delete</div>";
+            html = html + "<div class = 'boothcommentbutton secondcommentbutton' onclick= 'deleteComment(" + boothnum + ", " + obj.commentnum + ")'>Delete</div>";
         } else if (obj.commentername == window.username) {
-            html = html + "<div class =  \"boothcommentbutton \" onclick= \"deleteComment(" + boothnum + ", " + obj.commentnum + ") \">Delete</div>";
+            html = html + "<div class = 'boothcommentbutton' onclick= 'deleteComment(" + boothnum + ", " + obj.commentnum + ")'>Delete</div>";
         }
         if (obj.commentername != window.username) {
-            html = html + "<div class =  \"boothcommentlikebutton \" onclick= \"likeComment(" + obj.commentnum + ") \">Like</div>";
+            html = html + "<div class = 'boothcommentlikebutton' onclick= 'likeComment(" + obj.commentnum + ")'>Like</div>";
         }
         html = html +
             "</div>" +
