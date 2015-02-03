0c66d325cd39aad6a4b621fa88a05c69ddc9629d
Brad <bradsk88@gmail.com>
2015-02-02 23:15:30 -0600

    Added v2 endpoints for friend feed, public feed and user feed. Made booth thumbnails less rounded.  Fuck Web 2.0

---
+++ _mobile/v2/friendfeed.php
@@ -0,0 +1,99 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Brad
+ * Date: 5/21/14
+ * Time: 9:51 PM
+ */
+
+error_reporting(0);
+session_start();
+main();
+
+function main()
+{
+
+    require_once("{$_SERVER['DOCUMENT_ROOT']}/userpages/friendbooth_utils.php");
+    require_once("{$_SERVER['DOCUMENT_ROOT']}/_mobile/utils.php");
+    require_once("{$_SERVER['DOCUMENT_ROOT']}/common/boiler.php");
+    require_common("db");
+    require_common("utils");
+
+    $link = connect_to_boothsite();
+    update_online_presence();
+
+    $username = $_POST['username'];
+    if (isset($_SESSION['username'])) {
+        $username = $_SESSION['username'];
+    } else if (isset($_POST['username']) && failsStandardMobileChecksAndEchoFailureMessage()) {
+        return;
+    }
+    $_SESSION['username'] = $username;
+
+    if (!userExists($username)) {
+        echo json_encode(
+            array(
+                "error" => "Current user '" . $username . "' does not exist"));
+        return;
+    }
+
+    if (isBanned($username)) {
+        echo json_encode(
+            array(
+                "error" => "User is banned"));
+        return;
+    }
+
+    $sql = getSQL();
+    if ($sql == -1) {
+        return;
+    }
+
+    $result = mysql_query($sql);
+
+    if (!$result) {
+        echo json_encode(
+            array(
+                "error" => mysql_death1($sql)));
+        return;
+    }
+
+    $booths = array();
+    while ($row = mysql_fetch_array($result)) {
+        $root = "http://" . $_SERVER['SERVER_NAME'];
+        $booths[] = array(
+            'boothnum' => $row['pkNumber'],
+            'boothername' => $row['fkUsername'],
+            'bootherdisplayname' => (string)getDisplayName($row['fkUsername']),
+            'blurb' => $row['blurb'],
+            'imageHash' => $row['imageTitle'],
+            'filetype' => $row['filetype'],
+            'absoluteImageUrlThumbnail' => $root . '/booths/small/' . $row['imageTitle'] . '.' . $row['filetype']);
+        $newestBooth = $row['pkNumber'];
+    }
+    echo json_encode(
+        array("success" =>
+            array(
+                "booths" => $booths),
+            "next_batch_start_booth_number" => $newestBooth
+        )
+    );
+}
+
+function getSQL()
+{
+    $pageNum = 1;
+    if (isset($_POST['pagenum'])) {
+        $pageNum = $_POST['pagenum'];
+    }
+
+    $numberOfPages = 10;
+    if (isset($_POST['numberofbooths'])) { //backwards compatibility
+        $numberOfPages = $_POST['numberofbooths'];
+    }
+    if (isset($_POST['numperpage'])) {
+        $numberOfPages = $_POST['numperpage'];
+    }
+
+    return getFriendBoothsSQL($_SESSION['username'], $pageNum, $numberOfPages);
+}
---
+++ _mobile/v2/publicfeed.php
@@ -0,0 +1,96 @@
+<?php
+
+    session_start();
+    error_reporting(0);
+    main();
+
+    function main() {
+
+        require_once("{$_SERVER['DOCUMENT_ROOT']}/livefeed/utils.php");
+        require_once("{$_SERVER['DOCUMENT_ROOT']}/_mobile/utils.php");
+        require_once("{$_SERVER['DOCUMENT_ROOT']}/common/boiler.php");
+        require_common("db");
+        require_common("utils");
+
+        $link = connect_to_boothsite();
+        update_online_presence();
+
+        $sql = getSQL();
+        $result = mysql_query($sql);
+
+        if (!$result) {
+            echo json_encode(
+                array(
+                    "error" => mysql_death1($sql)
+                )
+            );
+            return;
+        }
+
+        $booths = array();
+        $newestBooth = -1;
+        while($row = mysql_fetch_array($result)) {
+            $root = "http://" . $_SERVER['SERVER_NAME'];
+            $booths[] = array(
+                'boothnum' => $row['pkNumber'],
+                'boothername' => $row['fkUsername'],
+                'bootherdisplayname' => (string)getDisplayName($row['fkUsername']),
+                'blurb' => $row['blurb'],
+                'imageHash' => $row['imageTitle'],
+                'filetype' => $row['filetype'],
+                'absoluteImageUrlThumbnail' => $root . '/booths/small/' . $row['imageTitle'] . '.' . $row['filetype'],
+                'absoluteImageUrl' => $root . '/booths/' . $row['imageTitle'] . '.' . $row['filetype']);
+            $newestBooth = $row['pkNumber'];
+        }
+        echo json_encode(
+            array(
+                "success" => array(
+                    "booths" => $booths,
+                    "next_batch_start_booth_number" => $newestBooth
+                )
+            )
+        );
+
+    }
+
+function getSQL()
+{
+    $pageNum = 1;
+    if (isset($_POST['pagenum'])) {
+        $pageNum = $_POST['pagenum'];
+    }
+
+    $numPerPage = 9;
+    if (isset($_POST['numperpage'])) {
+        $numPerPage = $_POST['numperpage'];
+    }
+
+    if (isset($_SESSION['username'])) {
+        return getMyPublicFeedSQL($_SESSION['username'], $pageNum, $numPerPage);
+    }
+
+    $newerThanBoothNumber = -1;
+    if (isset($_POST['newer_than_booth_number'])) {
+        $newerThanBoothNumber = $_POST['newer_than_booth_number'];
+    }
+
+    if (isset($_POST['username'], $_POST['phoneid'], $_POST['loginkey'])) {
+        $username = $_POST['username'];
+        $_SESSION['username'] = $username;
+        $check = isKeyOK($username, $_POST['phoneid'], $_POST['loginkey']);
+        if ($check == OK) {
+            return getMyPublicFeedSQL($username, $pageNum, $numPerPage, $newerThanBoothNumber);
+
+        }
+    }
+    return getPublicFeedSQL($pageNum, $newerThanBoothNumber);
+}
+
+function getMyPublicFeedSQL($username, $pageNum, $numPerPage, $newerThanBoothNumber=-1) {
+    if (isset($_POST['includeFriends'])) {
+        if ($_POST['includeFriends'] == false || $_POST['includeFriends'] = "false") {
+           return getNonFriendPublicFeedSQL($username, $pageNum, $numPerPage, $newerThanBoothNumber);
+        }
+    }
+    return getUserPublicFeedSQL($username, $pageNum, $numPerPage, 0, $newerThanBoothNumber);
+}
