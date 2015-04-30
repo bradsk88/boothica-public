a1e71b95d28c95a8af11554a9451e80473fdf505
Brad Johnson <bradsk88@gmail.com>
2015-04-29 21:10:17 -0600

    Initial commit of PM page.  No functionality.

--- pm/index.php
+++ pm/index.php
@@ -3,17 +3,16 @@ require_once("{$_SERVER['DOCUMENT_ROOT']}/common/boiler.php");
 require_once("{$_SERVER['DOCUMENT_ROOT']}/framing/PageFrame.php");
 require_once("{$_SERVER['DOCUMENT_ROOT']}/pages/LoginPage.php");
 require_lib("h2o-php/h2o");
+require_common("internal_utils");
 
 class PrivateMessagePage extends PageFrame {
 
     function __construct() {
         parent::__construct();
-        $htmlBuilder = new h2o("{$_SERVER['DOCUMENT_ROOT']}/pm/templates/index.mst");
-        $html = $htmlBuilder->render(array(
+        parent::setBodyTemplateAndValues("{$_SERVER['DOCUMENT_ROOT']}/pm/templates/index.mst", array(
             "baseUrl" => base(),
             "requestHash" => generateUserUniqueHash($_SESSION['username'])
         ));
-        $this->body($html);
     }
 
 }
