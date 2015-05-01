98822af03ecfd2eddeffe54a4ab9822e5b71bdc9
Brad Johnson <bradsk88@gmail.com>
2015-04-30 22:48:51 -0600

    Created a roughed out static pm page

--- pm/index.php
+++ pm/index.php
@@ -4,6 +4,7 @@ require_once("{$_SERVER['DOCUMENT_ROOT']}/framing/PageFrame.php");
 require_once("{$_SERVER['DOCUMENT_ROOT']}/pages/LoginPage.php");
 require_lib("h2o-php/h2o");
 require_common("internal_utils");
+require_asset("UserImage");
 
 class PrivateMessagePage extends PageFrame {
 
@@ -20,6 +21,12 @@ class PrivateMessagePage extends PageFrame {
 if (isLoggedIn()) {
     $page = new PrivateMessagePage();
     $page->css("http://fonts.googleapis.com/css?family=Bitter:400,700");
+    $page->css(base()."/css/pm.css");
+    $htmlBuilder = new h2o("{$_SERVER['DOCUMENT_ROOT']}/pm/templates/privateMessage.mst");
+    $html = $htmlBuilder->render(array(
+        "userImageUrl" => UserImage::getAbsoluteImage("roze")
+    ));
+    $page->body($html);
     $page->echoHtml();
 } else {
     $page = new LoginPage();
