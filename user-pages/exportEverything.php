9dd2dec63df6e8dc30164f52cb11c545016b687f
Brad Johnson <bradmin@boothi.ca>
2016-01-04 23:01:36 -0600

    Initial commit of "The End"

---
+++ user-pages/exportEverything.php
@@ -0,0 +1,7 @@
+<?PHP
+require_once "{$_SERVER['DOCUMENT_ROOT']}/common/boiler.php";
+require_once "{$_SERVER['DOCUMENT_ROOT']}/pages/InfoPage.php";
+
+$page = new InfoPage("The End", "This page will, eventually, supply the ability to export your booths.
+<br/><br/>Stay tuned.");
+$page->echoHtml();
